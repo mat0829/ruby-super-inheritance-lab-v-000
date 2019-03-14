4fa9bfc18d974dbb77d1e4fcb744f250753e8a27
Mat Anderson <matandersonlv@hotmail.com>
2019-03-14 23:03:06 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,7 +8,7 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n" to stdout, but output "Pick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n\nPick me!\n"
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
   end
   
 end
